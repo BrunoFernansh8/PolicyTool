a75acef6c8d68800cefc4f4abeb77287be74e1ef
safian_123 <2646240h@student.gla.ac.uk>
2025-01-06 15:26:14 +0000

    Revert "Created Dockerfile"
    This reverts commit 88c990e2ad1e388dbfd027bd0e236b6acb9281a7.

--- backend/server.js
+++ backend/server.js
@@ -11,9 +11,7 @@ app.use(express.json());
 mongoose.connect('mongodb://localhost:27017/cloudRiskDB', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
-})
-.then(() => console.log('Connected to MongoDB'))
-.catch(err => console.error('MongoDB connection error:', err));
+});
 
 // Routes
 app.use('/api/auth', authenticateRoutes);
